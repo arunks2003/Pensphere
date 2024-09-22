1e7233d08ea600b30ea7ee9e4542bdbf15f18222
Arun Kumar <arun.kumar.cd.civ21@itbhu.ac.in>
2024-09-22 12:15:02 +0530

    remove bug in postcards

--- src/file-components/blogPostCard/BlogPostCard.jsx
+++ src/file-components/blogPostCard/BlogPostCard.jsx
@@ -8,25 +8,41 @@ const BlogPostCard = () => {
     const { mode, getAllBlog, user } = context;
     // console.log(user.email);
     const navigate = useNavigate();
-
+    const htmlToPlainText = (html) => {
+        // Create a new DOMParser instance
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(html, 'text/html');
+        return doc.body.textContent || "";
+    };
+    function createMarkup(c) {
+        return { __html: c };
+    }
     return (
         <>
             {getAllBlog.length > 0 ? <>{
                 getAllBlog.map((item, index) => {
                     const { thumbnail, id, date } = item
                     console.log(item)
+                    const plainText = htmlToPlainText(item.blogs.content);
+                    let c;
+                    c = plainText;
                     return (
-                        <div className="max-w-sm rounded overflow-hidden shadow-lg hover:cursor-pointer hover:shadow-2xl hover:scale-105 transition-transform duration-300" key={index}>
-                            <img onClick={() => navigate(`/bloginfo/${id}`)} className="w-full" src={thumbnail} alt="blog" />
+                        <div className="max-w-sm rounded overflow-hidden shadow-lg hover:cursor-pointer hover:shadow-2xl hover:scale-105 transition-transform duration-300" key={index} style={{ height: '400px' }}>
+                            <img
+                                onClick={() => navigate(`/bloginfo/${id}`)}
+                                className="w-full h-48 object-cover"
+                                src={thumbnail}
+                                alt="blog"
+                            />
                             <div className="px-6 py-4">
-                                <div className="font-bold text-xl mb-2">{item.blogs.title}</div>
-                                <p className="text-gray-700 text-base">
-                                    {item.blogs.content}
+                                <div className="font-bold text-xl mb-2 truncate">{item.blogs.title}</div>
+                                <p className="text-gray-700 text-base overflow-hidden h-16" dangerouslySetInnerHTML={createMarkup(item.blogs.content)}>
+                                    {/* {c}  Adjust this height to manage text content overflow */}
                                 </p>
                             </div>
                             <div className="px-6 pt-4 pb-2">
                                 <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#{item.blogs.category}</span>
-                                <p className='text-gray-700 mr-2 mb-2'>by: {item.blogs.author}</p>
+                                <p className="text-gray-700 text-sm mr-2">by: {item.blogs.author}</p>
                             </div>
                         </div>
 
